9a82104a930eae5871acb2e997a73828de3f1dc9
Alex <dtk077@gmail.com>
2015-05-03 15:43:32 +0300

    Update parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,43 +11,55 @@
 # Sample Usage:
 #
 class marathon(
-  # Install or uninstall (present|absent)
+# Install or uninstall (present|absent)
   $installation_ensure      = 'present',
-  # Marathon binary url
+# Marathon binary url
   $url                      = 'https://downloads.mesosphere.io/marathon/v0.8.2-RC1/marathon-0.8.2-RC1.tgz',
-  # Marathon binary digest url
+# Marathon binary digest url
   $digest_url             = 'https://downloads.mesosphere.io/marathon/v0.8.2-RC1/marathon-0.8.2-RC1.tgz.sha256',
-  # The digest type
+# The digest type
   $digest_type            = 'sha256',
-  # Temporary directory to download the files to
+# Temporary directory to download the files to
   $tmp_dir                  = '/tmp',
-  # Marathon Installation directory
+# Marathon Installation directory
   $install_dir              = '/opt/marathon',
-  # The username that marathon will submit tasks as
+# The username that marathon will submit tasks as
   $user                     = 'root',
-  # Whether or not to create scripts in /usr/local/bin
+# Whether or not to create scripts in /usr/local/bin
   $create_symlinks          = true,
-  # Create symlinks for the marathon binaries for easier access
+# Create symlinks for the marathon binaries for easier access
   $haproxy_discovery        = false,
-  # Create and manage the marathon service
+# Create and manage the marathon service
   $manage_service           = true,
-  # The marathon service's name
+# The marathon service's name
   $service_name             = 'marathon',
-  # Manage the firewall rules
+# The marathon options
+  $options                  = hiera('classes::marathon::options', {}),
+# Manage the firewall rules
   $manage_firewall          = false,
-  # Manage the user that the tasks will be submitted as
+# Manage the user that the tasks will be submitted as
   $manage_user              = true,
-  # Whether or not the integrity of the archive should be verified
+# Whether or not the integrity of the archive should be verified
   $checksum                 = true,
-  # Global haproxy options
-  $haproxy_default_options  = hiera('haproxy::global_options', false),
-  # Default HAproxy options
-  $haproxy_default_options  = hiera('haproxy::default_options', false)
+# Global haproxy options
+  $haproxy_global_options   = hiera('classes::haproxy::global_options', {}),
+# Default HAproxy options
+  $haproxy_default_options  = hiera('classes::haproxy::default_options', {})
 ) {
 
   validate_bool($create_symlinks, $manage_service, $manage_firewall, $manage_user, $haproxy_discovery, $checksum)
   validate_path($tmp_dir, $install_dir)
   validate_string($url, $digest_url, $user)
   validate_re($installation_ensure, '^(present|absent)$',"${installation_ensure} is not supported for installation_ensure. Allowed values are 'present' and 'absent'.")
+  validate_hash($options, $haproxy_global_options, $haproxy_default_options)
 
+  if $options != undef and $options['HTTP_ADDRESS'] != undef {
+    if  !has_interface_with('ipaddress', $options['HTTP_ADDRESS']) {
+      fail('The specified IP does not belong to this host.')
+    }
+  }
+
+  anchor{ 'marathon::install::start': } ->
+  class { 'marathon::install': } ->
+  anchor { 'marathon::install::end': }
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,37 +1,39 @@
 # Marathon installation class
 
 class marathon::install (
-  # Install or uninstall (present|absent)
+# Install or uninstall (present|absent)
   $installation_ensure     = 'present',
-  # Marathon binary url
+# Marathon binary url
   $url                     = $marathon::url,
-  # Marathon binary digest url
+# Marathon binary digest url
   $digest_url            = $marathon::digest_url,
-  # The digest type
+# The digest type
   $digest_type            = 'sha256',
-  # Temporary directory to download the files to
+# Temporary directory to download the files to
   $tmp_dir                 = $marathon::tmp_dir,
-  # Marathon Installation directory
+# Marathon Installation directory
   $install_dir             = $marathon::install_dir,
-  # The username that marathon will submit tasks as
+# The username that marathon will submit tasks as
   $user                    = $marathon::user,
-  # Whether or not to create scripts in /usr/local/bin
+# Whether or not to create scripts in /usr/local/bin
   $create_symlinks         = $marathon::create_symlinks,
-  # Create symlinks for the marathon binaries for easier access
+# Create symlinks for the marathon binaries for easier access
   $haproxy_discovery       = $marathon::haproxy_discovery,
-  # Create and manage the marathon service
+# Create and manage the marathon service
   $manage_service          = $marathon::manage_service,
-  # The marathon service's name
+# The marathon service's name
   $service_name             = $marathon::service_name,
-  # Manage the firewall rules
+# The marathon options
+  $options                  = $marathon::options,
+# Manage the firewall rules
   $manage_firewall         = $marathon::manage_firewall,
-  # Manage the user that the tasks will be submitted as
+# Manage the user that the tasks will be submitted as
   $manage_user             = $marathon::manage_user,
-  # Whether or not the integrity of the archive should be verified
+# Whether or not the integrity of the archive should be verified
   $checksum                = $marathon::checksum,
-  # Global haproxy options
-  $haproxy_default_options = hiera('haproxy::global_options', false),
-  # Default HAproxy options
+# Global haproxy options
+  $haproxy_global_options  = $marathon::haproxy_global_options,
+# Default HAproxy options
   $haproxy_default_options = $marathon::haproxy_default_options
 ) inherits marathon {
 
@@ -39,6 +41,30 @@ class marathon::install (
   validate_path($tmp_dir, $install_dir)
   validate_string($url, $digest_url, $user)
   validate_re($installation_ensure, '^(present|absent)$',"${installation_ensure} is not supported for installation_ensure. Allowed values are 'present' and 'absent'.")
+
+  if $options != undef and $options['HTTP_ADDRESS'] != undef {
+    if  !has_interface_with('ipaddress', $options['HTTP_ADDRESS']) {
+      fail('The specified IP does not belong to this host.')
+    }
+  }
+
+  if $manage_user == true and !defined(User[$user]) and !defined(Group[$user]) and $user != 'root' {
+    group { $user:
+      ensure => present,
+      name   => $user
+    }
+    user { $user:
+      ensure     => present,
+      managehome => true,
+      shell      => '/sbin/nologin',
+      require    => [Group[$user]],
+      groups     => [$user,'root']
+    }
+  } elsif  $manage_user == true and !defined(User[$user]) and $user == 'root' {
+    user { $user:
+      ensure     => present
+    }
+  }
 
   ensure_resource('archive', 'marathon', {
     ensure           => present,
@@ -52,4 +78,6 @@ class marathon::install (
     digest_url       => $digest_url,
     digest_type      => $digest_type
   })
+
+
 }
