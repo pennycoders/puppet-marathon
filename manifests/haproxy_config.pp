f26dbbc65ed63e0fa56e747ada417244a91c6ee4
Alex <dtk077@gmail.com>
2015-05-18 21:19:11 +0300

    Add consul support

--- manifests/haproxy_config.pp
+++ manifests/haproxy_config.pp
@@ -1,42 +1,42 @@
 class marathon::haproxy_config (
-# Install or uninstall (present|absent)
-  $installation_ensure      = $marathon::installation_ensure,
-# Marathon binary url
-  $url                      = $marathon::url,
-# Marathon binary digest string
-  $digest_string            = $marathon::digest_string,
-# The digest type
-  $digest_type              = $marathon::digest_type,
-# Temporary directory to download the files to
-  $tmp_dir                  = $marathon::tmp_dir,
-# Marathon Installation directory
-  $install_dir              = $marathon::install_dir,
-# The username that marathon will submit tasks as
-  $user                     = $marathon::user,
-# Create symlinks for the marathon binaries for easier access
-  $create_symlinks          = $marathon::create_symlinks,
-#  Whether to use haproxy for load balancing between services
-  $haproxy_discovery        = false,
-# Create and manage the marathon service
-  $manage_service           = $marathon::manage_service,
-# The marathon service's name
-  $service_name             = $marathon::service_name,
-# The marathon options
-  $options                  = $marathon::options,
-# Manage the firewall rules
-  $manage_firewall          = $marathon::manage_firewall,
-# Manage the user that the tasks will be submitted as
-  $manage_user              = $marathon::manage_user,
-# Whether or not the integrity of the archive should be verified
-  $checksum                 = $marathon::checksum,
-#  Whether or not to use consul (http://consul.io) for service discovery
-  $consul_discovery         = $marathon::consul_discovery,
-#  Consul configuration
-  $consul_options           = $marathon::consul_options,
-# Whether to install consul-template or not
-  $install_consul_template  = $marathon::install_consul_template,
-#  consul-template options
-  $consul_template_options  = $marathon::consul_template_options
+      # Install or uninstall (present|absent)
+        $installation_ensure      = $marathon::installation_ensure,
+      # Marathon binary url
+        $url                      = $marathon::url,
+      # Marathon binary digest string
+        $digest_string            = $marathon::digest_string,
+      # The digest type
+        $digest_type              = $marathon::digest_type,
+      # Temporary directory to download the files to
+        $tmp_dir                  = $marathon::tmp_dir,
+      # Marathon Installation directory
+        $install_dir              = $marathon::install_dir,
+      # The username that marathon will submit tasks as
+        $user                     = $marathon::user,
+      # Create symlinks for the marathon binaries for easier access
+        $create_symlinks          = $marathon::create_symlinks,
+      #  Whether to use haproxy for load balancing between services
+        $haproxy_discovery        = false,
+      # Create and manage the marathon service
+        $manage_service           = $marathon::manage_service,
+      # The marathon service's name
+        $service_name             = $marathon::service_name,
+      # The marathon options
+        $options                  = $marathon::options,
+      # Manage the firewall rules
+        $manage_firewall          = $marathon::manage_firewall,
+      # Manage the user that the tasks will be submitted as
+        $manage_user              = $marathon::manage_user,
+      # Whether or not the integrity of the archive should be verified
+        $checksum                 = $marathon::checksum,
+      #  Whether or not to use consul (http://consul.io) for service discovery
+        $consul_discovery         = $marathon::consul_discovery,
+      #  Consul configuration
+        $consul_options           = $marathon::consul_options,
+      # Whether to install consul-template or not
+        $install_consul_template  = $marathon::install_consul_template,
+      #  consul-template options
+        $consul_template_options  = $marathon::consul_template_options
 ) inherits marathon {
 
   validate_bool($create_symlinks, $manage_service, $manage_firewall, $manage_user, $haproxy_discovery, $consul_discovery, $checksum, $install_consul_template)
@@ -51,9 +51,17 @@ class marathon::haproxy_config (
     }
   }
 
-  package { 'haproxy':
-    ensure => 'present'
+  if $haproxy_discovery == true {
+    ensure_resource('package','haproxy',{
+      ensure => 'latest'
+    })
   }
 
+  if $consul_discovery == true {
+    ensure_resource('class', 'consul', $consul_options)
+  }
 
+  if $install_consul_template == true {
+    ensure_resource('class', 'consul_template', $consul_template_options)
+  }
 }
