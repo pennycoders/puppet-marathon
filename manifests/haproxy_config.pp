ef87b9c5bd7737d26397b9b9bd6422e3aadd3b4f
Alex <dtk077@gmail.com>
2015-05-19 18:35:02 +0300

    Update registrator config

--- manifests/haproxy_config.pp
+++ manifests/haproxy_config.pp
@@ -105,7 +105,7 @@ class marathon::haproxy_config (
   if $install_registrator == true and $consul_discovery and $install_consul_template and is_hash($consul_options['config_hash']) and $consul_options['config_hash']['client_addr'] {
   ensure_resource('docker::run','registrator', {
       image           => 'gliderlabs/registrator:latest',
-      command         => "-ip ${consul_options['config_hash']['client_addr']} consul://${consul_options['config_hash']['client_addr']}:${consul_template_options['consul_port']}",
+      command         => "-ip ${consul_options['config_hash']['client_addr']} consul://${consul_options['config_hash']['client_addr']}:${consul_template_options['consul_port']} -resync 10 -ttl 10 -ttl-refresh 20 -deregister always -internal",
       use_name        => true,
       volumes         => ["${docker_socket_bind}:/tmp/docker.sock"],
       memory_limit    => '10m',
