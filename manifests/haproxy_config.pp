cf7f38e45b736aa2f7910b22dddbff709886c5b6
Alex <dtk077@gmail.com>
2015-05-29 04:41:35 +0300

    Update manifest

--- manifests/haproxy_config.pp
+++ manifests/haproxy_config.pp
@@ -303,15 +303,20 @@ class marathon::haproxy_config (
     }
   }
 
+  $registratorInterestedParties = defined(Service['consul']) ? {
+    true  => Service['consul'],
+    false => undef
+  }
   if $install_registrator == true and $consul_discovery == true and $install_consul_template == true and is_hash($consul_options['config_hash']) and $consul_options['config_hash']['client_addr'] {
     ensure_resource('docker::run','registrator', {
-  image           => 'gliderlabs/registrator:latest',
-  command         => "-ip ${consul_options['config_hash']['client_addr']} consul://${consul_options['config_hash']['client_addr']}:${consul_template_options['consul_port']} -resync ${registrator_resync} ${registrator_args}",
-  use_name        => true,
-  volumes         => ["${docker_socket_bind}:/tmp/docker.sock"],
-  memory_limit    => '10m',
-  hostname        => $::fqdn,
-  pull_on_start   => true
+      image           => 'gliderlabs/registrator:latest',
+      notify          => $registratorInterestedParties,
+      command         => "-ip ${consul_options['config_hash']['client_addr']} consul://${consul_options['config_hash']['client_addr']}:${consul_template_options['consul_port']} -resync ${registrator_resync} ${registrator_args}",
+      use_name        => true,
+      volumes         => ["${docker_socket_bind}:/tmp/docker.sock"],
+      memory_limit    => '10m',
+      hostname        => $::fqdn,
+      pull_on_start   => true
   })
 }
 
